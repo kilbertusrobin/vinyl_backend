17cd185004f0adf6175001605cf6af0d1ef4b5eb
yanisboidin <yanisboidin@laposte.net>
2025-06-25 11:39:24 +0200

    [add] (init) swagger

---
+++ src/config/swagger.config.ts
@@ -0,0 +1,17 @@
+import { INestApplication } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+
+export function swagger(app: INestApplication): string {
+  const swaggerPath = 'documentation';
+
+  const config = new DocumentBuilder()
+    .setTitle('Vinyl API')
+    .setDescription('The vinyl API description')
+    .setVersion('1.0')
+    .addTag('vinyl')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup(swaggerPath, app, document);
+
+  return swaggerPath;
+}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { swagger } from './config/swagger.config';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const swaggerPath = swagger(app);
+  const port = process.env.PORT ?? 3000;
+
+  await app.listen(port);
+  Logger.log(
+    `Documentation is available at http://localhost:${port}/${swaggerPath}`,
+    'Swagger',
+  );
 }
 bootstrap();
